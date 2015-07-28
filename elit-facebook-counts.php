27c954a90f9dab86f744faac8e4e365b852691f1
pjsinco <psinco@fas.harvard.edu>
2015-07-28 18:18:38 -0500

    first commit

---
+++ elit-facebook-counts.php
@@ -0,0 +1,102 @@
+<?php 
+
+/*
+Plugin Name: Facebook Counts
+Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Get counts of Facebook likes, shares and comments for each post.
+Version:     0.0.1
+Author:      PJS
+Author URI:  http://github.com/pjsinco
+License:     GPL2
+*/
+
+// if this file is called directly, abort
+if (!defined('WPINC')) {
+    die;
+}
+
+define('FB_URL', "http://api.facebook.com/restserver.php?method=links." . 
+    "getStats&urls=");
+
+/**
+ * Get all id, title and date of all published posts
+ *
+ * @return array of post IDs
+ * @author PJ
+ */
+function elit_fb_get_posts()
+{
+    global $wpdb;
+    //$table = $wpdb->prefix . '_posts';
+    $q = "
+        SELECT id, post_name, post_date
+        FROM {$wpdb->prefix}posts
+        WHERE post_status = 'publish'
+          AND post_type = 'post'
+        ORDER BY id
+    ";
+    
+    $results = $wpdb->get_results($q);
+
+    return $results;
+}
+
+
+/**
+ * Add a menu page for our plugin to the admin menu.
+ *
+ */
+function elit_fb_counts_menu() {
+
+    add_options_page(
+        'Facebook Counts',
+        'Facebook Counts',
+        'manage_options',
+        'facebook-counts',
+        'elit_fb_counts_options_page'
+    );
+
+}
+add_action('admin_menu' , 'elit_fb_counts_menu');
+
+function elit_fb_counts_options_page() {
+
+    if (!current_user_can('manage_options')) {
+        wp_die('You do not have sufficient permission to access this page.');
+    }
+
+    require('includes/options-page-wrapper.php');
+
+}
+
+
+function elit_fb_activation() {
+
+    if (!wp_next_scheduled('elit_fb_cron_hook')) {
+        wp_schedule_event(time(), 'daily', 'elit_fb_cron_hook');
+    }
+
+}
+register_activation_hook(__FILE__, 'elit_fb_activation');
+
+
+function elit_fb_process_posts() {
+
+    $posts = elit_fb_get_posts();
+
+    if ($posts) {
+        foreach ($posts as $post) {
+            $stats = array();
+            $url = FB_URL . urlencode(get_permalink($post->id));
+            $info = wp_remote_get($url);
+            $xml = simplexml_load_string($info['body']);
+            $stats['elit_fb_shares'] = (string) $xml->link_stat->share_count;
+            $stats['elit_fb_likes'] = (string) $xml->link_stat->like_count;
+            $stats['elit_fb_comments'] = (string) $xml->link_stat->comment_count;
+    
+            update_post_meta($post->id, 'elit_fb', serialize($stats));
+        }
+    }
+
+}
+add_action('elit_fb_cron_hook' , 'elit_fb_process_posts');
---
+++ includes/options-page-wrapper.php
@@ -0,0 +1,65 @@
+<h3><?php _e( '2 Column Layout: static (px) reducing of toggle block', 'wp_admin_style' ); ?></h3>
+
+<div class="wrap">
+
+    <div id="icon-options-general" class="icon32"></div>
+    <h2><?php esc_attr_e( 'Heading String', 'wp_admin_style' ); ?></h2>
+
+    <div id="poststuff">
+
+        <div id="post-body" class="metabox-holder columns-2">
+
+            <!-- main content -->
+            <div id="post-body-content">
+
+                <div class="meta-box-sortables ui-sortable">
+
+                    <div class="postbox">
+
+                        <h3><span>Main Content Header</span></h3>
+
+                        <div class="inside">
+                            <p>Lorem ipsum</p>
+                        </div>
+                        <!-- .inside -->
+
+                    </div>
+                    <!-- .postbox -->
+
+                </div>
+                <!-- .meta-box-sortables .ui-sortable -->
+
+            </div>
+            <!-- post-body-content -->
+
+            <!-- sidebar -->
+            <div id="postbox-container-1" class="postbox-container">
+
+                <div class="meta-box-sortables">
+
+                    <div class="postbox">
+
+                        <h3><span>Sidebar Content Header</span></h3>
+
+                        <div class="inside">
+                            <p>lorem</p>
+                        </div>
+                        <!-- .inside -->
+
+                    </div>
+                    <!-- .postbox -->
+
+                </div>
+                <!-- .meta-box-sortables -->
+
+            </div>
+            <!-- #postbox-container-1 .postbox-container -->
+
+        </div>
+        <!-- #post-body .metabox-holder .columns-2 -->
+
+        <br class="clear">
+    </div>
+    <!-- #poststuff -->
+
+</div> <!-- .wrap -->
